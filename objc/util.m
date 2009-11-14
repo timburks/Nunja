ff092c0302bac21923f919b0480c42c4d83d684c
Tim Burks <tim@neontology.com>
2009-11-14 09:05:39 -0800

    Separated some Nunja-specific helpers from helpers.m (refactoring)

---
+++ objc/util.m
@@ -0,0 +1,83 @@
+/*!
+@file util.m
+@discussion General utilities for the Nunja web server.
+@copyright Copyright (c) 2008 Neon Design Technology, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
+#include <syslog.h>
+#include <string.h>
+
+#import <Foundation/Foundation.h>
+
+// http://www.netzmafia.de/skripten/unix/linux-daemon-howto.html
+void daemonize()
+{
+    pid_t pid, sid;
+
+    /* Fork off the parent process */
+    pid = fork();
+    if (pid < 0) {
+        exit(EXIT_FAILURE);
+    }
+    /* If we got a good PID, then
+       we can exit the parent process. */
+    if (pid > 0) {
+        exit(EXIT_SUCCESS);
+    }
+
+    /* Change the file mode mask */
+    umask(0);
+
+    /* Open any logs here */
+
+    /* Create a new SID for the child process */
+    sid = setsid();
+    if (sid < 0) {
+        /* Log any failures here */
+        exit(EXIT_FAILURE);
+    }
+
+    /* Change the current working directory */
+    if ((chdir("/")) < 0) {
+        /* Log any failures here */
+        exit(EXIT_FAILURE);
+    }
+
+    /* Close out the standard file descriptors */
+    close(STDIN_FILENO);
+    close(STDOUT_FILENO);
+    close(STDERR_FILENO);
+}
+
+@implementation NSFileManager (Nunja)
+
+- (BOOL) directoryExistsAtPath:(NSString *) path
+{
+    BOOL isDirectory = NO;
+    BOOL fileExists = [self fileExistsAtPath:path isDirectory:&isDirectory];
+    if (!fileExists)
+        return NO;
+    else
+        return isDirectory;
+}
+
+@end
